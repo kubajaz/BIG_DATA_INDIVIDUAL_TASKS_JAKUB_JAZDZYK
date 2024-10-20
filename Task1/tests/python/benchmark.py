f0b106c1f5de427792fc59b45435623f7e82c393
kuba <kuba.jazdzyk@gmail.com>
2024-10-20 23:21:38 +0100

    .

--- Task1/tests/python/benchmark.py
+++ Task1/tests/python/benchmark.py
@@ -21,24 +21,20 @@ def test_multiply_matrices(benchmark):
     results = []
 
     for n in sizes:
-        # Benchmarking
         exec_time = benchmark(multiply_matrices, n)
 
-        # Mierzenie użycia pamięci i CPU
-        memory_usage = psutil.virtual_memory().used / (1024 ** 2)  # MB
+        memory_usage = psutil.virtual_memory().used / (1024 ** 2)
         cpu_usage = psutil.cpu_percent(interval=None)
 
         results.append({
             "Matrix Size": n,
-            "Execution Time (s)": exec_time,  # Użyj .mean dla średniego czasu
+            "Execution Time (s)": exec_time,
             "Memory Usage (MB)": memory_usage,
             "CPU Usage (%)": cpu_usage
         })
 
-        # Poprawiono drukowanie exec_time
         print(f"Size: {n}, Time: {exec_time:.6f}s, Memory: {memory_usage:.2f}MB, CPU: {cpu_usage:.2f}%")
 
-    # Zapisz wyniki do pliku CSV
     save_results_to_csv(results)
 
 def save_results_to_csv(results, filename='benchmark_results.csv'):
@@ -47,4 +43,4 @@ def save_results_to_csv(results, filename='benchmark_results.csv'):
     print(f"Results saved to {filename}")
 
 if __name__ == "__main__":
-    pytest.main([__file__, "--benchmark-only"])  # Uruchom benchmark, jeśli plik jest uruchamiany bezpośrednio
+    pytest.main([__file__, "--benchmark-only"]) 
